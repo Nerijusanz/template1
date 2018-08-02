000d861f76e6936b0c35d8a2e4727d6428da02c8
Nerijus Anužis <nerijusanz@gmail.com>
2018-08-02 13:50:14 +0300

    webpack npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,22 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
+//webpack:
+//npm i -D webpack webpack-cli
+
+/*NPM*/
+//js es6 dependency
+/*npm install --save-dev babel-core babel-loader babel-preset-env*/
+
+//add .babelrc file; {"presets": ["env"]}
+
+//css autoprefixer:
+/*npm i -D postcss-loader autoprefixer*/
+
+//plugins:
+//npm i -D extract-text-webpack-plugin@next
+//npm i -D copy-webpack-plugin
+
 module.exports = {
   entry: ['./src/js/main.js'],
   output: {
